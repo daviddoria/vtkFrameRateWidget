a1340f4a95ba236ba73a204052791eaac6038cdb
Benoist LAURENT <benoist@ibpc.fr>
2010-10-19 16:41:53 +0200

    Add code for VTK version 5.3 to 5.6

--- vtkFrameRateWidget.cxx
+++ vtkFrameRateWidget.cxx
@@ -8,31 +8,68 @@
 #include "vtkTextRepresentation.h"
 #include "vtkTextProperty.h"
 
+
 #include <sstream>
+
+
+/* VTK version stuff */
+#define OLD_VTK_VERSION (VTK_MAJOR_VERSION<5 || (VTK_MAJOR_VERSION==5 && VTK_MINOR_VERSION<7))
+#if OLD_VTK_VERSION
+
+#include "vtkCallbackCommand.h"
+
+static void RenderCallbackOLD(vtkObject *caller, unsigned long, void *clientData, void *)
+{
+	vtkRenderer *renderer = vtkRenderer::SafeDownCast(caller);
+	vtkFrameRateWidget *frw = (vtkFrameRateWidget*) clientData;
+	
+	double timeInSeconds = renderer->GetLastRenderTimeInSeconds();
+	double fps = 1.0/timeInSeconds;
+	
+	std::stringstream ss;
+	ss << "Frame rate: " << fps << " (fps)";
+	frw->GetTextActor()->SetInput(ss.str().c_str());	
+}
+#endif
+
 
 vtkStandardNewMacro(vtkFrameRateWidget);
 
 void vtkFrameRateWidget::Init()
 {
-  this->Renderer->AddObserver(vtkCommand::EndEvent, this, &vtkFrameRateWidget::RenderCallback);
+#if OLD_VTK_VERSION
+	vtkCallbackCommand *callback = vtkCallbackCommand::New();
+	callback->SetCallback(RenderCallbackOLD);
+	callback->SetClientData(this);
+	this->Renderer->AddObserver(vtkCommand::EndEvent, callback);
+	callback->Delete();
+#else
+	this->Renderer->AddObserver(vtkCommand::EndEvent, this, &vtkFrameRateWidget::RenderCallback);
+#endif
 
-  this->CreateDefaultRepresentation();
-  static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPositionCoordinate()->SetCoordinateSystemToNormalizedViewport();
-  static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPosition2Coordinate()->SetCoordinateSystemToNormalizedViewport();
-  static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPositionCoordinate()->SetValue(.05, .05);
-  static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPosition2Coordinate()->SetValue(.3, .2);
-  //static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetTextActor()->GetTextProperty()->SetColor( 0.0, 1.0, 0.0 );
-  this->GetTextActor()->GetTextProperty()->SetColor( 0.0, 1.0, 0.0 );
-  this->GetTextActor()->SetInput("Frame rate: ");
+	
+	cout << OLD_VTK_VERSION << endl;
+	
+	this->CreateDefaultRepresentation();
+	static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPositionCoordinate()->SetCoordinateSystemToNormalizedViewport();
+	static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPosition2Coordinate()->SetCoordinateSystemToNormalizedViewport();
+	static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPositionCoordinate()->SetValue(.05, .05);
+	static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetPosition2Coordinate()->SetValue(.3, .2);
+	//static_cast<vtkTextRepresentation*>(this->WidgetRep)->GetTextActor()->GetTextProperty()->SetColor( 0.0, 1.0, 0.0 );
+	this->GetTextActor()->GetTextProperty()->SetColor( 0.0, 1.0, 0.0 );
+	this->GetTextActor()->SetInput("Frame rate: ");
 }
 
 void vtkFrameRateWidget::RenderCallback(vtkObject* caller, long unsigned int vtkNotUsed(eventId),
                                         void* vtkNotUsed(callData) )
 {
-  double timeInSeconds = this->Renderer->GetLastRenderTimeInSeconds();
-  double fps = 1.0/timeInSeconds;
-
-  std::stringstream ss;
-  ss << "Frame rate: " << fps << " (fps)";
-  this->GetTextActor()->SetInput(ss.str().c_str());
+	double timeInSeconds = this->Renderer->GetLastRenderTimeInSeconds();
+	double fps = 1.0/timeInSeconds;
+	
+	std::stringstream ss;
+	ss << "Frame rate: " << fps << " (fps)";
+	this->GetTextActor()->SetInput(ss.str().c_str());
 }
+
+
+#undef OLD_VTK_VERSION
